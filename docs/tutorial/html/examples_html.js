d6c0d32acbf21fd0b617ab5f45b0af2acd83127d
Mr_Diamond123 <kurtisiscool123@gmail.com>
2017-01-20 10:09:48 -0500

    Update examples_html.js

--- docs/tutorial/html/examples_html.js
+++ docs/tutorial/html/examples_html.js
@@ -1,3 +1,19 @@
-function example(id) {
+//Example Vars
+var examp1 = `
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>I am a title!</title>
+  </head>
 
-}
+  <body>
+  <h1>I am Mr. Heading 1!</h1>
+  <p>I am a paragraph!</p>
+  </body>
+</html>
+
+`
+
+//Example HTML Elements
+document.getElementById("example_1").innerHTML = 
+  

b3784b159194c2b45421d0a4ee13547db05287b6
Mr_Diamond123 <kurtisiscool123@gmail.com>
2017-01-20 10:33:53 -0500

    Update examples_html.js

--- docs/tutorial/html/examples_html.js
+++ docs/tutorial/html/examples_html.js
@@ -15,5 +15,18 @@ var examp1 = `
 `;
 
 //Example HTML Elements
-document.getElementById("example_1").innerHTML = examp1;
-console.log(examp1);  
+document.getElementById("example_1").innerHTML = `
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>I am a title!</title>
+  </head>
+
+  <body>
+  <h1>I am Mr. Heading 1!</h1>
+  <p>I am a paragraph!</p>
+  </body>
+</html>
+
+`;
+
